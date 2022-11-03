55ee23c15fc9ed3867df0b6fbd0dc4c753827b2c
Tera <24725862+teraa@users.noreply.github.com>
2022-11-03 23:53:48 +0100

    add parser test

---
+++ Common.Tests/PlayerParserTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Tests;
+
+public class PlayerParserTests
+{
+    [Fact]
+    public void Parse()
+    {
+        var player = PlayerParser.Parse("1,GK,Mendy,Chelsea,140,6.0");
+        player.Should().Be(new Player(1, Position.GK, "Mendy", "Chelsea", 140, 6.0m));
+    }
+}
