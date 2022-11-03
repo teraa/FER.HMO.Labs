471a8bfe5bd009867adc849d776d31d0a3616d60
Tera <24725862+teraa@users.noreply.github.com>
2022-11-03 22:09:08 +0100

    create InstanceLoader

---
+++ Common/InstanceLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Common;
+
+public static class InstanceLoader
+{
+    public static IReadOnlyList<Player> LoadFromFile(string fileName, Encoding? encoding = null)
+    {
+        using var stream = File.OpenRead(fileName);
+        return LoadFromStream(stream, encoding);
+    }
+
+    public static IReadOnlyList<Player> LoadFromStream(Stream stream, Encoding? encoding = null)
+    {
+        using var reader = new StreamReader(stream, encoding ?? Encoding.UTF8);
+
+        var players = new List<Player>();
+        while (reader.ReadLine() is { } line)
+        {
+            var player = PlayerParser.Parse(line);
+            players.Add(player);
+        }
+
+        return players;
+    }
+}
+
