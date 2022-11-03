60926d74dadd7fc09e29d316ef814d19b8c17b79
Tera <24725862+teraa@users.noreply.github.com>
2022-11-03 22:08:39 +0100

    add tests

---
+++ Lab1.Tests/Tests.cs
@@ -0,0 +1,80 @@
+namespace Lab1.Tests;
+
+public abstract class Tests
+{
+    protected Solution Solution { get; init; } = null!;
+
+    [Fact]
+    public void Squad_PlayerCount()
+        => Solution.Squad
+            .Should().HaveCount(15);
+
+    [Fact]
+    public void FirstTeam_PlayerCount()
+        => Solution.FirstTeam
+            .Should().HaveCount(11);
+
+    [Fact]
+    public void FirstTeam_SubsetOf_Squad()
+        => Solution.FirstTeam.Should().BeSubsetOf(Solution.Squad);
+
+    [Fact]
+    public void Squad_Cost()
+        => Solution.Squad.Sum(x => x.Price)
+            .Should().BeLessThanOrEqualTo(100m);
+
+    [Fact]
+    public void Squad_MaxPlayersPerClub()
+    {
+        using var scope = new AssertionScope();
+        foreach (var clubPlayers in Solution.Squad.GroupBy(x => x.Club))
+        {
+            scope.FailWith($"Squad has more than 3 players from club \"{clubPlayers.Key}\".");
+            clubPlayers.Should().HaveCountLessThanOrEqualTo(3);
+        }
+    }
+
+    [Fact]
+    public void Squad_GK_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.GK)
+            .Should().HaveCount(2);
+
+    [Fact]
+    public void Squad_DEF_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.DEF)
+            .Should().HaveCount(5);
+
+    [Fact]
+    public void Squad_MID_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.MID)
+            .Should().HaveCount(5);
+
+    [Fact]
+    public void Squad_FW_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.FW)
+            .Should().HaveCount(3);
+
+    [Fact]
+    public void FirstTeam_GK_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.GK)
+            .Should().HaveCount(1);
+
+    [Fact]
+    public void FirstTeam_DEF_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.DEF)
+            .Should().HaveCountGreaterThanOrEqualTo(3)
+            .And.HaveCountLessThanOrEqualTo(5);
+
+    [Fact]
+    public void FirstTeam_MID_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.MID)
+            .Should().HaveCountGreaterThanOrEqualTo(2)
+            .And.HaveCountLessThanOrEqualTo(5);
+
+    [Fact]
+    public void FirstTeam_FW_Count()
+        => Solution.FirstTeam.Where(x => x.Position == Position.FW)
+            .Should().HaveCountGreaterThanOrEqualTo(1)
+            .And.HaveCountLessThanOrEqualTo(3);
+
+}
---
+++ Lab1.Tests/Usings.cs
@@ -0,0 +1,5 @@
+global using Common;
+global using FluentAssertions;
+global using FluentAssertions.Execution;
+global using JetBrains.Annotations;
+global using Xunit;
