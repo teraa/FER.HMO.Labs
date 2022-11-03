3fcf63672aabfc530ceb105fefda4ddddcae9d32
Tera <24725862+teraa@users.noreply.github.com>
2022-11-03 22:07:13 +0100

    create Lab1 project

---
+++ Lab1/GreedySolver.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace Lab1;
+
+public class GreedySolver : ISolver
+{
+    public Solution Solve(IEnumerable<Player> players)
+    {
+        throw new NotImplementedException();
+    }
+}
