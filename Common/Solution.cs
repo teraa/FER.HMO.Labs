6977ea68c76fb75dafecf7fbd5300ddee6217f20
Tera <24725862+teraa@users.noreply.github.com>
2022-11-04 00:48:20 +0100

    solution record

--- Common/Solution.cs
+++ Common/Solution.cs
@@ -1,12 +1,5 @@
 ﻿namespace Common;
 
-public class Solution
-{
-    public Solution(IReadOnlyList<Player> squad)
-    {
-        Squad = squad;
-    }
-
-    public IReadOnlyList<Player> Squad { get; }
-    public IEnumerable<Player> FirstTeam => Squad.Take(11);
-}
+public record Solution(
+    IReadOnlyList<Player> Squad,
+    IReadOnlyList<Player> FirstTeam);
