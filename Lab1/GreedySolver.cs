adfbee81c61d5f875fb19c740e032de3abf9540f
Tera <24725862+teraa@users.noreply.github.com>
2022-11-04 20:09:42 +0100

    implement GreedySolver

--- Lab1/GreedySolver.cs
+++ Lab1/GreedySolver.cs
@@ -4,7 +4,29 @@ namespace Lab1;
 
 public class GreedySolver : ISolver
 {
+    private static readonly IReadOnlyList<Formation> s_validFormations;
+    private static readonly Formation s_squadFormation = new(2, 5, 5, 3);
+
+    static GreedySolver()
+    {
+        var formations = new List<Formation>();
+
+        int gk = 1;
+        for (int def = 3; def <= 5; def++)
+        for (int mid = 0; mid <= 5; mid++)
+        for (int fw = 1; fw <= 3; fw++)
+        {
+            if (gk + def + mid + fw != 11)
+                continue;
+
+            formations.Add(new Formation(new[] {gk, def, mid, fw}));
+        }
+
+        s_validFormations = formations;
+    }
+
     private readonly IReadOnlyList<Player> _instance;
+    private State _state = null!;
 
     public GreedySolver(IReadOnlyList<Player> instance)
     {
@@ -13,6 +35,57 @@ public class GreedySolver : ISolver
 
     public Solution Solve()
     {
-        throw new NotImplementedException();
+        _state = new State();
+
+        var players = _instance.OrderByDescending(x => x.Points / x.Price);
+        foreach (var player in players)
+        {
+            if (_state.Squad.Count == 15)
+                break;
+
+            if (CanPickForSquad(player))
+                _state.Squad.Add(player);
+        }
+
+        foreach (var player in _state.Squad)
+        {
+            if (_state.FirstTeam.Count == 11)
+                break;
+
+            if (CanPickForFirstTeam(player))
+                _state.FirstTeam.Add(player);
+        }
+
+        var solution = new Solution(_state.Squad, _state.FirstTeam);
+        return solution;
+    }
+
+    private bool CanPickForSquad(Player player)
+    {
+        var players = _state.Squad.Concat(new[] {player}).ToList();
+        if (players.Sum(x => x.Price) > 100m)
+            return false;
+
+        if (players.Count(x => x.Club == player.Club) > 3)
+            return false;
+
+        var formation = Formation.FromPlayers(players);
+
+        return formation.Values.Zip(s_squadFormation.Values, (x, y) => x <= y).All(x => x);
+    }
+
+    private bool CanPickForFirstTeam(Player player)
+    {
+        var players = _state.FirstTeam.Concat(new[] {player});
+        var formation = Formation.FromPlayers(players);
+
+        return s_validFormations.Any(validFormation =>
+            formation.Values.Zip(validFormation.Values, (x, y) => x <= y).All(x => x));
+    }
+
+    private class State
+    {
+        public List<Player> Squad { get; } = new();
+        public List<Player> FirstTeam { get; } = new();
     }
 }
