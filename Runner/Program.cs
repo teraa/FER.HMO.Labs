35ae00146b11f89ccfe98c6e7172d6ae63c5307f
Tera <24725862+teraa@users.noreply.github.com>
2022-11-04 20:20:18 +0100

    add runner

---
+++ Runner/Program.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+using Lab1;
+
+foreach (var fileName in new[] {"2022_instance1.csv", "2022_instance2.csv", "2022_instance3.csv"})
+{
+    var instance = InstanceLoader.LoadFromFile(fileName);
+    var solver = new GreedySolver(instance);
+    var solution = solver.Solve();
+    var message =
+        $"Instance: {fileName}\n" +
+        $"Squad: {string.Join(',', solution.Squad.Select(x => x.Id))}\n" +
+        $"First team: {string.Join(',', solution.FirstTeam.Select(x => x.Id))}\n" +
+        $"Squad cost: {solution.Squad.Sum(x => x.Price)}\n" +
+        $"First team points: {solution.FirstTeam.Sum(x => x.Points)}\n";
+    Console.WriteLine(message);
+}
