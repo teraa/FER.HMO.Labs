e1cc88f2098fb9ac604ebdd2dc9563187589405e
Tera <24725862+teraa@users.noreply.github.com>
2022-11-04 20:09:28 +0100

    create Formation

---
+++ Common/Formation.cs
@@ -0,0 +1,42 @@
+﻿namespace Common;
+
+public class Formation
+{
+    private readonly int[] _formation;
+
+    public Formation(int[] formation)
+    {
+        if (formation is null)
+            throw new ArgumentNullException(nameof(formation));
+        if (formation.Length != 4)
+            throw new ArgumentOutOfRangeException(nameof(formation));
+
+        _formation = formation;
+    }
+
+    public Formation(int gk, int def, int mid, int fw)
+    {
+        _formation = new[] {gk, def, mid, fw};
+    }
+
+    public int this[Position position]
+    {
+        get => _formation[(int) position];
+        private set => _formation[(int) position] = value;
+    }
+
+    public IReadOnlyList<int> Values
+        => _formation;
+
+    public override string ToString()
+        => string.Join(',', _formation);
+
+    public static Formation FromPlayers(IEnumerable<Player> players)
+    {
+        var formation = new Formation(0, 0, 0, 0);
+        foreach (var player in players)
+            formation[player.Position]++;
+
+        return formation;
+    }
+}
