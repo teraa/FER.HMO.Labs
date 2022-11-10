ec1527650bfd243b8e8c8e2da944ff58f00dbd9e
Tera <24725862+teraa@users.noreply.github.com>
2022-11-10 20:41:15 +0100

    update runner

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -4,26 +4,37 @@ using Lab1;
 int i = 0;
 foreach (var func in new Func<Player, double>[]
          {
-             x => x.Points,
-             x => (double) (x.Points / x.Price),
+             // x => x.Points,
+             // x => (double) (x.Points / x.Price),
              x => Math.Pow(x.Points, 2) / (double) x.Price,
-             x => x.Points / Math.Log10((double) x.Price),
-             x => Math.Pow(x.Points, 2.25) / (double) x.Price,
+             // x => Math.Pow(x.Points, 2.25) / (double) x.Price,
+             // x => x.Points / Math.Log10((double) x.Price + 1),
          })
 {
     var solutions = new List<Solution>();
 
-    foreach (var fileName in new[]
-             {
-                 "2022_instance1.csv", "2022_instance2.csv", "2022_instance3.csv",
-                 "2021_instance1.csv"
-             })
+    string dir = args is {Length: 1}
+        ? args[0]
+        : Environment.CurrentDirectory;
+
+    var files = Directory.GetFiles(dir, "*.csv");
+
+    if (files.Length == 0)
     {
-        var instance = InstanceLoader.LoadFromFile(fileName);
+        Console.Error.WriteLine("No instance files found.");
+        return;
+    }
+
+    foreach (var file in files)
+    {
+        var fileName = Path.GetFileName(file);
+
+        var instance = InstanceLoader.LoadFromFile(file);
         var solver = new GraspSolver(instance)
+        // var solver = new GreedySolver(instance)
         {
             PlayerValue = func,
-            Alpha = 0,
+            Alpha = 0.2,
         };
         var solution = solver.Solve();
         solutions.Add(solution);
