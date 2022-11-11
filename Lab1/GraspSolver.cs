1b94e871949e86c80dd9e9d4897115c40853a468
Tera <24725862+teraa@users.noreply.github.com>
2022-11-11 11:09:24 +0100

    impl ls

--- Lab1/GraspSolver.cs
+++ Lab1/GraspSolver.cs
@@ -7,6 +7,7 @@ public class GraspSolver : ISolver
 {
     private readonly IReadOnlyList<Player> _instance;
     private readonly Random _random = new();
+    private int _lsCount;
 
     public GraspSolver(IReadOnlyList<Player> instance)
     {
@@ -14,7 +15,7 @@ public class GraspSolver : ISolver
     }
 
     public Func<Player, double> PlayerValue { get; set; } = x => Math.Pow(x.Points, 2) / (double) x.Price;
-    public double Alpha { get; set; } = 0.5;
+    public double Alpha { get; set; } = 0.2;
     public int Iterations { get; set; } = 10;
 
     public Solution Solve()
@@ -24,17 +25,24 @@ public class GraspSolver : ISolver
             .ToList();
 
         var solutions = new List<PartialSolution>();
+        var maxLsCount = 0;
 
         for (int k = 0; k < Iterations; k++)
         {
             var solution = Construct(players);
 
-            LocalSearch(solution);
+            _lsCount = 0;
+            solution = LocalSearch(solution, players);
+            maxLsCount = Math.Max(_lsCount, maxLsCount);
 
             Debug.Assert(solution is {Squad.Count: 15, FirstTeam.Count: 11});
 
             solutions.Add(solution);
         }
+
+        #if DEBUG
+        Console.WriteLine($"{_instance.Count}, {maxLsCount}");
+        #endif
 
         var bestSolution = solutions.MaxBy(x => x.Value)!;
         return bestSolution.ToSolution();
@@ -52,7 +60,7 @@ public class GraspSolver : ISolver
             var eligiblePlayers = players
                 .Except(solution.Squad)
                 // .Skip(i == 0 ? 0 : players.IndexOf(solution.Squad.Last()) + 1)
-                .Where(x => solution.CanAddToSquad(x))
+                .Where(solution.CanAddToSquad)
                 .ToList();
 
             double max = PlayerValue(eligiblePlayers[0]);
@@ -79,11 +87,58 @@ public class GraspSolver : ISolver
                 solution.FirstTeam.Add(player);
         }
 
+        // Greedy reduce squad cost
+        var substitutes = solution.Squad.Except(solution.FirstTeam).ToList();
+        Debug.Assert(substitutes is {Count: 4});
+        foreach (var substitute in substitutes)
+        {
+            solution.Squad.Remove(substitute);
+
+            var newSubstitute = players
+                .Except(solution.Squad)
+                .Where(solution.CanAddToSquad)
+                .MinBy(x => x.Price)!;
+
+            Debug.Assert(newSubstitute is { });
+            Debug.Assert(newSubstitute.Price <= substitute.Price);
+
+            solution.Squad.Add(newSubstitute);
+        }
+
         return solution;
     }
 
-    private void LocalSearch(PartialSolution solution)
+    private PartialSolution LocalSearch(PartialSolution previousSolution, IReadOnlyList<Player> players)
     {
-        // Local search
+        _lsCount++;
+
+        var bestSolution = previousSolution;
+
+        foreach (var player in previousSolution.FirstTeam)
+        {
+            var solution = previousSolution.Clone();
+            solution.FirstTeam.Remove(player);
+            solution.Squad.Remove(player);
+
+            var newPlayer = players
+                .Except(solution.Squad)
+                .Where(solution.CanAddToSquad)
+                .Where(solution.CanAddToFirstTeam)
+                .FirstOrDefault(x => x.Points > player.Points);
+
+            if (newPlayer is null)
+                continue;
+
+            solution.FirstTeam.Add(newPlayer);
+            solution.Squad.Add(newPlayer);
+
+            if (bestSolution.Value < solution.Value)
+            {
+                solution = LocalSearch(solution, players);
+                bestSolution = solution;
+            }
+        }
+
+        return bestSolution;
     }
 }
