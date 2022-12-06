a9e78340b1bad2c9e70a294e9b7474e5c84993e1
Tera <24725862+teraa@users.noreply.github.com>
2022-12-06 19:54:02 +0100

    implement default functions

--- Lab2/Lab2.Solvers/SaSolver.cs
+++ Lab2/Lab2.Solvers/SaSolver.cs
@@ -14,16 +14,57 @@ public class SaSolver : ISolver
         _instance = instance;
     }
 
-    public Func<IReadOnlyList<Player>, Solution> InitialSolutionProvider { get; set; }
-        = instance => new GreedySolver(instance).Solve();
+    public Func<IReadOnlyList<Player>, Solution> InitialSolutionProvider { get; set; } = GreedySolution;
 
     public double InitialTemperature { get; set; } = 100;
 
-    public Func<double, int, double> DecrementFunction { get; set; } = (t, _) => t * 0.5;
+    public Func<double, int, double> DecrementFunction { get; set; } = DefaultDecrement;
+
+    public Func<double, double, double, double> ProbabilityFunction { get; set; } = DefaultProbability;
 
     public Solution Solve()
     {
         var solution = InitialSolutionProvider(_instance);
         return solution;
+    }
+
+    public static Solution GreedySolution(IReadOnlyList<Player> instance)
+        => new GreedySolver(instance).Solve();
+
+    public static Solution RandomSolution(IReadOnlyList<Player> instance)
+        => new RandomSolver(instance).Solve();
+
+    public static double DefaultDecrement(double temperature, int iteration)
+        => temperature * 0.5;
+
+    /// <summary>
+    /// Accept if improving else accept with certain probability.
+    /// </summary>
+    public static double DefaultProbability(double temperature, double currentValue, double newValue)
+    {
+        // minimization
+        // < 0 better
+        // = 0 equal
+        // > 0 worse
+        var delta = newValue - currentValue;
+        if (delta <= 0)
+            return 1;
+
+        var exponent = -delta / temperature;
+        var p = Math.Exp(exponent);
+        return p;
+    }
+
+    /// <summary>
+    /// Accept improving neighbors also with a certain probability.
+    /// </summary>
+    public static double AlternativeProbability(double temperature, double currentValue, double newValue)
+    {
+        // minimization
+        var delta = newValue - currentValue;
+        var exponent = delta / temperature;
+        var e = Math.Exp(exponent);
+        var p = 1 / (1 + e);
+        return p;
     }
 }
