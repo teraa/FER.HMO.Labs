e2e87b7cd69513d9587bb99b07607ee72a0dae34
Tera <24725862+teraa@users.noreply.github.com>
2022-12-08 20:22:19 +0100

    add Value and Cost properties to Solution

--- Common/Solution.cs
+++ Common/Solution.cs
@@ -2,4 +2,8 @@ namespace Common;
 
 public record Solution(
     IReadOnlyList<Player> Squad,
-    IReadOnlyList<Player> FirstTeam);
+    IReadOnlyList<Player> FirstTeam)
+{
+    public int Value => FirstTeam.Sum(x => x.Points);
+    public decimal Cost => Squad.Sum(x => x.Price);
+}
