c0c1d88c4117c4f94573bd405aa6c5816ba7fadf
Tera <24725862+teraa@users.noreply.github.com>
2022-12-08 21:54:41 +0100

    copy LS to tabu

--- Lab2/Lab2.Solvers/TabuSolver.cs
+++ Lab2/Lab2.Solvers/TabuSolver.cs
@@ -12,6 +12,46 @@ public class TabuSolver : ISolver
     public Solution Solve(Instance instance)
     {
         var incumbent = InitialSolver.Solve(instance);
+        return LocalSearch(incumbent, instance, out var iterations);
+    }
+
+    private Solution LocalSearch(Solution incumbent, Instance instance, out int iterations)
+    {
+        iterations = 0;
+        return LocalSearchInternal(new SolutionBuilder(incumbent), instance, ref iterations).ToSolution();
+    }
+
+    private SolutionBuilder LocalSearchInternal(SolutionBuilder previous, Instance instance, ref int iterations)
+    {
+        iterations++;
+
+        var incumbent = previous;
+
+        foreach (var player in previous.FirstTeam)
+        {
+            var current = previous.Clone();
+            current.FirstTeam.Remove(player);
+            current.Squad.Remove(player);
+
+            var newPlayer = instance.Players
+                .Except(current.Squad)
+                .Where(current.CanAddToSquad)
+                .Where(current.CanAddToFirstTeam)
+                .FirstOrDefault(x => x.Points > player.Points);
+
+            if (newPlayer is null)
+                continue;
+
+            current.FirstTeam.Add(newPlayer);
+            current.Squad.Add(newPlayer);
+
+            if (incumbent.Value < current.Value)
+            {
+                current = LocalSearchInternal(current, instance, ref iterations);
+                incumbent = current;
+            }
+        }
+
         return incumbent;
     }
 }
