3b50940a1ba24337e3b01733feaaecb410daa81b
Tera <24725862+teraa@users.noreply.github.com>
2022-12-10 21:06:23 +0100

    initial tabu impl

--- Lab2/Lab2.Solvers/TabuSolver.cs
+++ Lab2/Lab2.Solvers/TabuSolver.cs
@@ -1,57 +1,80 @@
+using System.Diagnostics;
 using Common;
 using JetBrains.Annotations;
-using Lab1.Solvers;
 
 namespace Lab2.Solvers;
 
 [PublicAPI]
 public class TabuSolver : ISolver
 {
-    public ISolver InitialSolver { get; set; } = new GreedySolver();
+    public ISolver InitialSolver { get; set; } = new RandomSolver();
+    public int Tenure { get; set; } = 10;
 
     public Solution Solve(Instance instance)
     {
         var incumbent = InitialSolver.Solve(instance);
-        return LocalSearch(incumbent, instance, out var iterations);
+        return TabuSearch(incumbent, instance, out var iterations);
     }
 
-    private Solution LocalSearch(Solution incumbent, Instance instance, out int iterations)
+    private Solution TabuSearch(Solution initial, Instance instance, out int iteration)
     {
-        iterations = 0;
-        return LocalSearchInternal(new SolutionBuilder(incumbent), instance, ref iterations).ToSolution();
-    }
+        const int terminateAfter = 10;
 
-    private SolutionBuilder LocalSearchInternal(SolutionBuilder previous, Instance instance, ref int iterations)
-    {
-        iterations++;
-
+        int improvedAt = iteration = 0;
+        var previous = new SolutionBuilder(initial);
         var incumbent = previous;
+        var tabu = new TabuList<Player>(Tenure);
 
-        foreach (var player in previous.FirstTeam)
+        for (iteration = 0; iteration - improvedAt < terminateAfter; iteration++)
         {
-            var current = previous.Clone();
-            current.FirstTeam.Remove(player);
-            current.Squad.Remove(player);
+            SolutionBuilder? bestInIteration = null;
 
-            var newPlayer = instance.Players
-                .Except(current.Squad)
-                .Where(current.CanAddToSquad)
-                .Where(current.CanAddToFirstTeam)
-                .FirstOrDefault(x => x.Points > player.Points);
-
-            if (newPlayer is null)
-                continue;
-
-            current.FirstTeam.Add(newPlayer);
-            current.Squad.Add(newPlayer);
-
-            if (incumbent.Value < current.Value)
+            foreach (var removed in previous.FirstTeam)
             {
-                current = LocalSearchInternal(current, instance, ref iterations);
-                incumbent = current;
+                var current = previous.Clone();
+                current.FirstTeam.Remove(removed);
+                current.Squad.Remove(removed);
+
+                var bestEligible = instance.Players
+                    .Except(previous.Squad)
+                    .Except(tabu)
+                    .Where(current.CanAddToSquad)
+                    // .Where(current.CanAddToFirstTeam) // Unnecessary if we are modifying only ONE player
+                    .OrderByDescending(x => x.Points)
+                    .FirstOrDefault(x => x.Points >= removed.Points);
+
+                if (bestEligible is null)
+                    continue;
+
+                current.FirstTeam.Add(bestEligible);
+                current.Squad.Add(bestEligible);
+
+                if (bestInIteration is null ||
+                    bestInIteration.Value < current.Value)
+                {
+                    bestInIteration = current;
+                }
+            }
+
+            if (bestInIteration is { })
+            {
+                var diff = bestInIteration.Squad
+                    .Except(previous.Squad)
+                    .Single();
+                tabu.TryAdd(diff);
+
+                // always better than `previous` but not necessarily incumbent
+                previous = bestInIteration;
+
+                if (incumbent.Value < bestInIteration.Value)
+                {
+                    incumbent = bestInIteration;
+                    improvedAt = iteration;
+                }
             }
         }
 
-        return incumbent;
+        Debug.WriteLine($"[TABU] Completed in {iteration} iterations");
+        return incumbent.ToSolution();
     }
 }
