d6f8ec7051c02b69733782e40a1baca9481aadfa
Tera <24725862+teraa@users.noreply.github.com>
2022-12-12 00:01:05 +0100

    update runner

--- Lab2/Lab2.Runner/Program.cs
+++ Lab2/Lab2.Runner/Program.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿using System.Diagnostics;
+using Common;
 using Lab1.Solvers;
 using Lab2.Solvers;
 
@@ -15,12 +16,13 @@ var randomSolver = new RandomSolver {Seed = null};
 
 var solvers = new SolverInfo[]
 {
-    new("Greedy", greedySolver),
-    new("Tabu+Random", new TabuSolver {InitialSolver = randomSolver}),
-    new("Tabu+Greedy", new TabuSolver {InitialSolver = greedySolver}),
-    new("Grasp", new GraspSolver()),
+    new("Random", randomSolver),
     new("Sa+Random", new SaSolver {InitialSolver = randomSolver}),
+    // new("Tabu+Random", new TabuSolver {InitialSolver = randomSolver}),
+    new("Greedy", greedySolver),
     new("Sa+Greedy", new SaSolver {InitialSolver = greedySolver}),
+    // new("Tabu+Greedy", new TabuSolver {InitialSolver = greedySolver}),
+    new("Grasp", new GraspSolver()),
 };
 
 foreach (var file in files)
@@ -28,10 +30,14 @@ foreach (var file in files)
     var instance = InstanceLoader.LoadFromFile(file);
     Console.WriteLine($"Instance: {file}");
 
+    var sw = new Stopwatch();
     foreach (var solverInfo in solvers)
     {
+        sw.Restart();
         var solution = solverInfo.Solver.Solve(instance);
-        Console.WriteLine($"{solverInfo.Name}: {solution.Value} ({solution.Cost})");
+        sw.Stop();
+
+        Console.WriteLine($"[{sw.ElapsedMilliseconds,5}ms] {solverInfo.Name,15}: {solution.Value,4} ({solution.Cost,5})");
     }
 }
 
