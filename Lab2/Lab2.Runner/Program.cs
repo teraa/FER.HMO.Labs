c54fb492c79aa0c544c10d26a78a6bf565f4bf9d
Tera <24725862+teraa@users.noreply.github.com>
2022-12-12 03:31:21 +0100

    Revert "runner: best of 20"
    This reverts commit b9c6b58d22e100ba9661c2a901e54b5c32ef8433.

--- Lab2/Lab2.Runner/Program.cs
+++ Lab2/Lab2.Runner/Program.cs
@@ -16,13 +16,13 @@ var randomSolver = new RandomSolver {Seed = null};
 
 var solvers = new SolverInfo[]
 {
-    // new("Random", randomSolver),
+    new("Random", randomSolver),
     new("Sa+Random", new SaSolver {InitialSolver = randomSolver}),
     new("Tabu+Random", new TabuSolver {InitialSolver = randomSolver}),
-    // new("Greedy", greedySolver),
+    new("Greedy", greedySolver),
     new("Sa+Greedy", new SaSolver {InitialSolver = greedySolver}),
     new("Tabu+Greedy", new TabuSolver {InitialSolver = greedySolver}),
-    // new("Grasp", new GraspSolver()),
+    new("Grasp", new GraspSolver()),
 };
 
 foreach (var file in files)
@@ -33,28 +33,14 @@ foreach (var file in files)
     var sw = new Stopwatch();
     foreach (var solverInfo in solvers)
     {
-        Solution? bestSolution = null;
-
-        for (int i = 0; i < 20; i++)
-        {
-            sw.Restart();
-            var solution = solverInfo.Solver.Solve(instance);
-            sw.Stop();
-
-            if (bestSolution is null || solution.Value > bestSolution.Value)
-                bestSolution = solution;
-
-            Console.Write(".");
-        }
-
-        Console.WriteLine();
-
-        Debug.Assert(bestSolution is { });
+        sw.Restart();
+        var solution = solverInfo.Solver.Solve(instance);
+        sw.Stop();
 
         Console.WriteLine(
-            $"[{sw.ElapsedMilliseconds,5}ms] {solverInfo.Name,15}: {bestSolution.Value,4} ({bestSolution.Cost,5})"
-            + " " + string.Join(",", bestSolution.FirstTeam.Select(x => x.Id))
-            + "|" + string.Join(",", bestSolution.Substitutes.Select(x => x.Id)));
+            $"[{sw.ElapsedMilliseconds,5}ms] {solverInfo.Name,15}: {solution.Value,4} ({solution.Cost,5})"
+            + " " + string.Join(",", solution.FirstTeam.Select(x => x.Id))
+            + "|" + string.Join(",", solution.Substitutes.Select(x => x.Id)));
     }
 }
 
