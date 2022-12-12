3587d1e4044fb0f2812be6f4537ffcc961946e30
Tera <24725862+teraa@users.noreply.github.com>
2022-12-12 03:05:50 +0100

    add Substitutions prop

--- Common/Solution.cs
+++ Common/Solution.cs
@@ -6,4 +6,5 @@ public record Solution(
 {
     public int Value => FirstTeam.Sum(x => x.Points);
     public decimal Cost => Squad.Sum(x => x.Price);
+    public IEnumerable<Player> Substitutes => Squad.Except(FirstTeam);
 }
